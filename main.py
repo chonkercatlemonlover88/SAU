bfc7ee6c3423beb7ff73c2d5db792f9f58ff6682
chonkercatlemonlover88 <95939975+chonkercatlemonlover88@users.noreply.github.com>
2023-12-11 16:38:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,31 @@
-import subprocess
-import sys
+import numpy as np
+from scipy.linalg import block_diag
 
-def install(package):
-    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+# Определение матриц
+A = np.array([[1, 3, 6], [2, 4, 5], [5, 4, 3]])
+B = np.array([[1, 2, 3], [0, 6, 4], [9, 8, 1]])
+C = np.array([[1, 1, 0]])
 
-packages = ["numpy", "scipy"]
+# Вычисление матрицы управляемости
+W = np.hstack((B, np.dot(A, B), np.dot(np.dot(A, A), B)))
+print(f"Матрица управляемости W:\n{W}")
 
-for package in packages:
-    install(package)
+# Вычисление ранга матрицы управляемости
+rank_W = np.linalg.matrix_rank(W)
+print(f"Ранг матрицы управляемости: {rank_W}")
+
+# Вычисление матрицы наблюдаемости
+V = np.vstack((C, np.dot(C, A), np.dot(C, np.dot(A, A))))
+print(f"Матрица наблюдаемости V:\n{V}")
+
+# Вычисление ранга матрицы наблюдаемости
+rank_V = np.linalg.matrix_rank(V)
+print(f"Ранг матрицы наблюдаемости: {rank_V}")
+
+# Вычисление собственных значений матрицы A
+eigvals = np.linalg.eigvals(A)
+print(f"Собственные значения матрицы A: {eigvals}")
+
+# Проверка устойчивости системы
+is_stable = all(np.real(eigval) < 0 for eigval in eigvals)
+print(f"Система устойчива: {is_stable}")
